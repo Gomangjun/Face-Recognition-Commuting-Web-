d29d2b3c3273fd12b00d0d7e72b9321087ed9fe2
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-08-09 23:51:51 +0900

    [init] spring boot 프로젝트 생성

---
+++ BE/src/main/java/com/example/Face_Recognition_Commuting_Web/FaceRecognitionCommutingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.Face_Recognition_Commuting_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionCommutingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionCommutingWebApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/Face_Recognition_Commuting_Web/FaceRecognitionCommutingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Face_Recognition_Commuting_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionCommutingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
